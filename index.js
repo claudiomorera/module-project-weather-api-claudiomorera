e473d8f63a4cb2513574d0fc72040cd9daaf955f
Claudio Gadiel Morera Mejia <106361170+claudiomorera@users.noreply.github.com>
2024-01-14 12:09:27 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -24,11 +24,45 @@ async function moduleProject4() {
       ev.target.setAttribute('disabled', 'disabled');
       document.querySelector('#weatherWidget').style.display = 'none';
       document.querySelector('.info').textContent = "Fetching weather data...";
-    } catch (err) {
-      console.error("error, could not perform the task: ${err}");
+	  console.log(ev.target.value);
+	  //Four task (request)
+	  let url = `https://localhost:3003/api/weather?city=${city}`;
+	  const res = await axios.get(url);
+	  
+	  document.querySelector('#watherWidget').style.display = 'block';
+	  document.querySelector('.info').textContent = '';
+	  ev.removeAttribute('disabled');
+	  
+	  let {data} = res;
+	  
+	  document.querySelector('#apparentTemp div:nth-child(2)').textContent = `${data.current.apparent_temperature}°`
+	  document.querySelector('#todayDescription').textContent = descriptions.find(d =>[0] === data.current.weather_description)[1]
+	  document.querySelector('#todayStats div:nth-child(1)').textContent = `${data.current.temperature_min}°/${data.current.temperature_max}°`
+	  document.querySelector('#todayStats div:nth-child(2)').textContent = `Precipitation: ${data.current.precipitation_probability * 100}%`
+	  document.querySelector('#todayStats div:nth-child(3)').textContent = `Huminidity: ${data.current.huminidity}%`
+	  document.querySelector('#todayStats div:nth-child(4)').textContent = `Wind: ${data.current.wind_speed}m/s`
+	  
+	  data.forecast.dailt.forEach((day, idx) => {
+		  let card = document.querySelectorAll('.next-day')[idx]
+		  let weekDay = card.children[0]
+		  let apparent = card.children[1]
+		  let minMax = card.children[2]
+		  let precipit = card.children[3]
+		  
+		  weekDay.textContent = getWeekDay(day.date)
+		  apparent.textContent = description.find(d => d[0] === day_weather_description)[1]
+		  minMax.textContent = `${day.temperature_min}°/${day.temperature_max}°`
+		  precipit.textContent = `Precipitation:${day.precipitation_probability * 100}%`
+	  })
+	  document.querySelector('#location').firstElementChild.textContent = data.location.city
+	  
+    } catch (error) {
+      console.error("error, could not perform the task: ", error.message)
     }
   });
-  
+  function getWeekDay(date) {
+	  return date.toLocaleString('en-US', {weekday:'long'});
+  }
   // 👆 WORK WORK ABOVE THIS LINE 👆
 
 }
